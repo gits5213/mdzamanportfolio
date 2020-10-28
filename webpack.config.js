8b6562271e19fe70855f74a2c032a846808e3dd8
Md Zaman <md83shahnewaz@gmail.com>
2020-10-28 18:56:58 -0400

    Md's initial protfolio

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+    entry: {
+        main: './src/index.js'
+    },
+    output: {
+        filename: 'index.html',
+        path: path.resolve(__dirname, 'docs')
+    },
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    },
+   plugins: [ new CopyWebpackPlugin([{from: './src/lib/legacyLib.js'}])]
+};
