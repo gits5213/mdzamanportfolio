3a200b196ee7012cc4fa3d6813057408c78f1db4
Md Zaman <md83shahnewaz@gmail.com>
2020-10-28 19:38:37 -0400

    Updates

---
+++ precache-manifest.c208d556d6e2b8f6e2dd3fe5abe3f10b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a06848c1cc1d4675f74e641655a43412",
+    "url": "/mdzamanportfolio/index.html"
+  },
+  {
+    "revision": "9b881bac782139757a3f",
+    "url": "/mdzamanportfolio/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "4ae1d7602b95ed08659e",
+    "url": "/mdzamanportfolio/static/css/main.f3cdbfca.chunk.css"
+  },
+  {
+    "revision": "9b881bac782139757a3f",
+    "url": "/mdzamanportfolio/static/js/2.047d8a23.chunk.js"
+  },
+  {
+    "revision": "e6139f805ebc917050ae28ff20bd10e5",
+    "url": "/mdzamanportfolio/static/js/2.047d8a23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ae1d7602b95ed08659e",
+    "url": "/mdzamanportfolio/static/js/main.8e4a3217.chunk.js"
+  },
+  {
+    "revision": "a40a410c190dec23a868",
+    "url": "/mdzamanportfolio/static/js/runtime-main.99422cfc.js"
+  },
+  {
+    "revision": "aa08acda28d39c176b371c01a9539b44",
+    "url": "/mdzamanportfolio/static/media/Md_Zaman_Picture.aa08acda.png"
+  },
+  {
+    "revision": "a0a99baa74f41c2ef588cb0f3a045f2f",
+    "url": "/mdzamanportfolio/static/media/resume_md.a0a99baa.pdf"
+  }
+]);
