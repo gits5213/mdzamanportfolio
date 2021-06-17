c81ff455d4f2dca130255e6161d1459455e505a3
md shahnewaz zaman <mzaman@resqsoft.com>
2021-06-17 15:44:02 -0400

    updated

--- src/projects/restassured.jsx
+++ src/projects/restassured.jsx
@@ -36,7 +36,7 @@ class restassured extends Component {
             <a href="https://msz5213@bitbucket.org/msz5213/">
               <Button colored>BitBucket</Button>
             </a>
-            <a href="https://youtu.be/0A3TZUiNUk8">
+            <a href="https://youtu.be/O0m33VaUx34">
               <Button colored>Live Demo</Button>
             </a>
           </CardActions>
