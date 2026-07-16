f975e67f1cfbebc7feb4c16baf6ccf9504a380ba
md shahnewaz zaman <mzaman@resqsoft.com>
2026-07-16 08:57:46 -0400

    mouse mover added

---
+++ public/MouseMover.bat
@@ -0,0 +1,14 @@
+@echo off
+title Mouse Mover - Press Ctrl+C to Stop
+
+powershell -NoProfile -ExecutionPolicy Bypass -Command ^
+"Add-Type -AssemblyName System.Windows.Forms; ^
+Add-Type -AssemblyName System.Drawing; ^
+while ($true) { ^
+    $originalPosition = [System.Windows.Forms.Cursor]::Position; ^
+    $newPosition = [System.Drawing.Point]::new($originalPosition.X + 1, $originalPosition.Y); ^
+    [System.Windows.Forms.Cursor]::Position = $newPosition; ^
+    Start-Sleep -Milliseconds 300; ^
+    [System.Windows.Forms.Cursor]::Position = $originalPosition; ^
+    Start-Sleep -Seconds 60; ^
+}"
